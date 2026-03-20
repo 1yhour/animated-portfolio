07b6ca7a698649c2f0f02a795fa439ced303a1bc
LYHOUR SENG <lyhourcoding@gmail.com>
2026-03-20 18:07:46 +0700

    feat: change greeting text

--- src/data/greetings.ts
+++ src/data/greetings.ts
@@ -1,7 +1,7 @@
 import type { Greeting } from "../types/index";
 export const GREETINGS: Greeting[] = [
   { text: "ស្វាគមន៍", lang: "km", font: "font-[Noto_Serif_Khmer]" },
-  { text: "ようこそ", lang: "ja", font: "font-[Noto_Sans_JP]" },
+  { text: "欢迎", lang: "zh", font: "font-[Noto_Sans_SC]" },
   { text: "Bienvenue", lang: "fr", font: "font-[Raleway]" },
   { text: "Welcome", lang: "en", font: "font-[Raleway]" },
 ] as const;
